b6307fa037397898c1b6e904fe9c29d091b01cbd
matthieugrieger <matthieugrieger@gmail.com>
2013-08-14 21:11:28 -0700

    Minor changes

--- versebot/versebot.py
+++ versebot/versebot.py
@@ -13,13 +13,12 @@ from commenter import constructComment
 from time import sleep
 from re import findall
 
-configloader.startup()
 filterwarnings("ignore", category=DeprecationWarning) # Ignores DeprecationWarnings caused by PRAW
 filterwarnings("ignore", category=ResourceWarning) # Ignores ResourceWarnings when using pickle files. May need to look into this later, but it seems to work fine.
 
 print('Loading Bible translation...')
 try:
-    file = open(configloader.getPickle(), 'rb')
+    file = open(configloader.getBible(), 'rb')
     bible = pickle.load(file)
     print('Bible translation successfully loaded!')
 except:
@@ -37,15 +36,17 @@ except:
 
 if not os.path.exists('data'): # Makes data folder if it doesn't already exist
     os.makedirs('data')
+
+comment_ids = None
 try:
-    comment_ids = pickle.load(open('data/commentids.p', 'rb'))
+    comment_ids = pickle.load(open(configloader.getCommentIdFile(), 'rb'))
 except EOFError:
     comment_ids = set()
 
 while True:
     print('Starting next scan...')
     subreddit = r.get_subreddit(configloader.getSubreddits())
-    for submission in subreddit.get_hot(limit = configloader.getScanLimit()):
+    for submission in subreddit.get_hot(limit = 10):
         flat_comments = praw.helpers.flatten_tree(submission.comments)
         for comment in flat_comments:
             if comment.id not in comment_ids:
@@ -54,6 +55,7 @@ while True:
                     nextComment = constructComment(versesToFind, comment, bible)
                     comment_ids.add(comment.id)
                     comment_ids.add(nextComment)
-                    pickle.dump(comment_ids, open('data/commentids.p', 'wb')) # Dumps new comment ids to file
+                    print('Dumping new comment ids to file...')
+                    pickle.dump(comment_ids, open(configloader.getCommentIdFile(), 'wb')) # Dumps new comment ids to file
                 
-    sleep(configloader.getScanDelay()) # Waits 60 seconds between scans by default
+    sleep(30) # Waits 30 seconds between scans by default
