197d38c86f50db8b59d539f2156874a2ccc949b6
matthieugrieger <matthieugrieger@gmail.com>
2013-08-14 21:33:48 -0700

    Remove commented code

--- versebot/configloader.py
+++ versebot/configloader.py
@@ -1,14 +1,5 @@
 from os import environ
 
-#global bible, username, password, subreddits, commentfile
-
-#def getEnvVariables(): # Reduces load on Heroku a tiny bit (loads environment variables just once)
-#    bible = environ['BIBLE']
-#    username = environ['USERNAME']
-#    password = environ['PASSWORD']
-#    subreddits = environ['SUBREDDITS']
-#    commentfile = environ['COMMENTFILE']
-
 def getBible():
     return environ['BIBLE']
 
