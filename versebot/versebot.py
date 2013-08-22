df3e403b90055cdc629e773720cda96a2a3c0868
matthieugrieger <matthieugrieger@gmail.com>
2013-08-21 18:22:38 -0700

    Restructured code, now reads new comments

--- versebot/versebot.py
+++ versebot/versebot.py
@@ -9,9 +9,9 @@ import configloader
 import os
 import psycopg2
 import urllib.parse
+from commenter import constructComment
 from sys import exit
 from warnings import filterwarnings
-from commenter import constructComment
 from time import sleep
 from re import findall
 from os import environ
@@ -21,18 +21,26 @@ filterwarnings("ignore", category=ResourceWarning) # Ignores ResourceWarnings wh
 
 print('Loading Bible translations...')
 try:
-    nivfile = open(configloader.getNIV(), 'rb')
-    nivbible = pickle.load(nivfile)
-    esvfile = open(configloader.getESV(), 'rb')
-    esvbible = pickle.load(esvfile)
-    kjvfile = open(configloader.getKJV(), 'rb')
-    kjvbible = pickle.load(kjvfile)
-    nrsvfile = open(configloader.getNRSV(), 'rb')
-    nrsvbible = pickle.load(nrsvfile)
+    nivbible = pickle.load(open(configloader.getNIV(), 'rb'))
+    esvbible = pickle.load(open(configloader.getESV(), 'rb'))
+    kjvbible = pickle.load(open(configloader.getKJV(), 'rb'))
+    nrsvbible = pickle.load(open(configloader.getNRSV(), 'rb'))
     print('Bible translations successfully loaded!')
 except:
     print('Error while loading Bible translations. Make sure the environment vars point to correct paths.')
     exit()
+
+def getNIVBible():
+    return nivbible
+
+def getESVBible():
+    return esvbible
+
+def getKJVBible():
+    return kjvbible
+
+def getNRSVBible():
+    return nrsvbible
 
 print('Connecting to reddit...')
 try:
@@ -58,45 +66,44 @@ cur = conn.cursor()
 io = open('tmp.txt', 'w')
 cur.copy_to(io, 'commentids', sep='|')
 io.close()
-commentsAdded = False
 
-def findTranslation(commentText):
-    if 'niv' in commentText:
-        return nivbible, 'NIV'
-    elif 'esv' in commentText:
-        return esvbible, 'ESV'
-    elif 'kjv' in commentText:
-        return kjvbible, 'KJV'
-    elif 'nrsv' in commentText:
-        return nrsvbible, 'NRSV'
-    else:
-        return esvbible, 'ESV'
+commentsAdded = False
+lookupList = set()
 
 while True:
+
     if commentsAdded:
         io = open('tmp.txt', 'w')
         cur.copy_to(io, 'commentids', sep='|')
         io.close()
-    print('Starting next scan...')
+    # Removed print('Starting next scan...'). Filled up the Heroku logs too much.
     subreddit = r.get_subreddit(configloader.getSubreddits())
-    for submission in subreddit.get_hot(limit = 10):
-        flat_comments = praw.helpers.flatten_tree(submission.comments)
-        for comment in flat_comments:
-            if comment.id not in open('tmp.txt').read() and 'vb' in comment.body.lower():
-                versesToFind = findall(r'(?:\d\s)?\w+\s\d+:?\d*-?\d*', comment.body) # Uses regex to find verses in comment body
-                if (len(versesToFind) != 0):
-                    bible = findTranslation(comment.body.lower())[0]
-                    translation = findTranslation(comment.body.lower())[1]
-                    nextComment = constructComment(versesToFind, comment, bible, translation)
-                    if nextComment != False:
-                        print('Inserting new comment ids to database...')
-                        cur.execute("""INSERT INTO commentids VALUES (%s);""", (comment.id,))
-                        cur.execute("""INSERT INTO commentids VALUES (%s);""", (nextComment,))
-                        conn.commit()
-                        commentsAdded = True
-                    else:
-                        print('Inserting comment id of comment with invalid command...')
-                        cur.execute("""INSERT INTO commentids VALUES (%s);""", (comment.id,)) # Adds comment id with invalid command so it is ignored in the future
-                        conn.commit()
-                        commentsAdded = True
+    subreddit_comments = subreddit.get_comments()
+    for comment in subreddit_comments:
+        if comment.id not in open('tmp.txt').read():
+            versesToFind = findall(r'\[[\w\s:,-]+]', comment.body) # Uses regex to find verses in comment body
+            if (len(versesToFind) != 0):
+                for ver in versesToFind:
+                    nextVer = findall(r'(?:\d\s)?(?:\w+\s\w+\s\w+)?(?:\w+\s\w+\s\w+\s\w+)?\w+\s\d+:?\d*-?\d*(?:\s\w+)?', ver) # Regex to find valid commands in brackets. Looks really ugly, can probably be made better.
+                    lookupList.add(str(nextVer))
+
+                if len(lookupList) != 0:
+                    # nextComment = False when only chapter is specified, needs to be fixed
+                    nextComment = constructComment(lookupList, comment, nivbible, esvbible, kjvbible, nrsvbible)
+                else:
+                    nextComment = False
+
+                if nextComment != False:
+                    print('Inserting new comment ids to database...')
+                    cur.execute("""INSERT INTO commentids VALUES (%s);""", (comment.id,))
+                    cur.execute("""INSERT INTO commentids VALUES (%s);""", (nextComment,))
+                    conn.commit()
+                    commentsAdded = True
+                    lookupList.clear()
+                else:
+                    print('Inserting comment id of comment with invalid command...')
+                    cur.execute("""INSERT INTO commentids VALUES (%s);""", (comment.id,)) # Adds comment id with invalid command so it is ignored in the future
+                    conn.commit()
+                    commentsAdded = True
+                    lookupList.clear()
     sleep(30) # Waits 30 seconds between scans by default
