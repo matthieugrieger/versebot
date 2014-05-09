341ffc60a9d4a9c66c6d3038546afbb40dff25c8
Matthieu Grieger <matthieugrieger@gmail.com>
2014-05-08 19:20:45 -0700

    Refactor

--- versebot/verse.py
+++ versebot/verse.py
@@ -1,270 +1,141 @@
-from collections import OrderedDict
-from re import findall
-import booknames
+#---------------------#
+# VerseBot for reddit #
+# By Matthieu Grieger #
+#---------------------#
 
-# Class that holds the properties of a Bible verse. This includes the book, chapter,
-# verse (or range of verses), and translation. Also within this class are multiple
-# methods used by VerseBot.
+from re import findall, search
+import data
+import helpers
+
+# Class that holds the properties of a Bible verse, and various
+# operations that go along with them.
 class Verse:
-    
-    _verseData = list()
-    _ESV = OrderedDict()
-    _NIV = OrderedDict()
-    _NRSV = OrderedDict()
-    _KJV = OrderedDict()
-    _DRA = OrderedDict()
-    _Brenton = OrderedDict()
-    _JPS = OrderedDict()
-    _Nova = OrderedDict()
-    _NLT = OrderedDict()
-    _invalidComment = False
-
-    # Initializes Verse object with data from the command(s)
-    def __init__(self, verseList, comment, translations):
-        self._ESV = translations[0]
-        self._NIV = translations[1]
-        self._NRSV = translations[2]
-        self._KJV = translations[3]
-        self._DRA = translations[4]
-        self._Brenton = translations[5]
-        self._JPS = translations[6]
-        self._Nova = translations[7]
-        self._NLT = translations[8]
-        
-        for verse in verseList:
-            verseBookNum = booknames.getBookNumber(verse.lower())
-            if verseBookNum != False:
-                verseSubreddit = comment.permalink[24:comment.permalink.find('/', 24)]
-                verseBookName = booknames.getBookTitle(verseBookNum)
-                verseChapter = self.__getChapterAndVerse(verse.lower())[0]
-                verseSelection = self.__getChapterAndVerse(verse.lower())[1]
-                verseTranslation = self.__getBibleTranslation(verse.lower(), verseSubreddit, verseBookNum)
-                verseContent = self.__getVerseContent(verseBookNum, verseChapter, verseSelection, verseTranslation)
-
-                if verseContent != False:
-                    self._verseData.append((verseBookName, verseChapter, verseSelection, verseTranslation, verseContent, verseSubreddit))
-        
-        if len(self._verseData) == 0:
-            self._invalidComment = True
-
-        return
-
-    # Constructs reddit comment.
-    def getComment(self):
-        if not self._invalidComment:
-            comment = ''
-            for curVerData in self._verseData:
-                # These assignments technically aren't necessary, it just makes the code below a lot easier to
-                # understand.
-                book = curVerData[0]
-                chap = curVerData[1]
-                ver = curVerData[2]
-                translation = curVerData[3]
-                content = curVerData[4]
-                subreddit = curVerData[5]
-                contextLink = self.__getContextLink(book, chap, translation)
-
-                if ver != '0':
-                    comment += ('[**' + book + ' ' + chap + ':' + ver + ' (*' + 
-                                translation + '*)**](' + contextLink + ')\n>' + content) + '\n\n'
-                else:
-                    comment += ('[**' + book + ' ' + chap + ' (*' + translation + '*)**](' + 
-                                contextLink + ')\n>' + content) + '\n\n'
-
-            if 0 < len(comment) <= self.__getCharLimit():
-                comment += self.__getCommentFooter()
-                return comment
-            else:
-                comment = self.__getOverflowComment()
-                comment += self.__getCommentFooter()
-                return comment
-        else:
-            return False
-
-    # Clears contents of _verseData.
-    def clearVerses(self):
-        self._verseData.clear()
-        return
-
-    # Finds chapter number and verse number within current verse request.
-    def __getChapterAndVerse(self, verse):
-        chap = '0'
-        ver = '0'
-        if ':' in verse:
-            chapterAndVerse = str(findall(r'\d+:\d*(?:-\d+)?', verse))
-            chap = (chapterAndVerse.partition(':')[0])[2:]
-            ver = (chapterAndVerse.partition(':')[2])[:-2]
-        else:
-            chap = str(findall(r'\s\d+', verse)).lstrip(' ')
-            chap = chap[3:-2]
-        
-        return chap, ver
-
-    # Determines the correct Bible translation to use for the current verse. It first looks for a user-specified
-    # translation. If there is no translation specified, it will then use the default translation for the subreddit
-    # in which the comment was posted.
-    def __getBibleTranslation(self, commentText, subreddit, book):
-        if 67 <= book <= 88: # Uses KJV bible if an Deuterocanon chapter is selected
-            if subreddit == 'OrthodoxChristianity':
-                return 'Brenton\'s Septuagint'
-            else:
-                return 'KJV Deuterocanon'
-        else:
-            if 'niv' in commentText:
-                return 'NIV'
-            elif 'esv' in commentText:
-                return 'ESV'
-            elif 'kjv' in commentText:
-                return 'KJV'
-            elif 'nrsv' in commentText:
-                return 'NRSV'
-            elif 'dra' in commentText or 'duoay' in commentText or 'rheims' in commentText:
-                return 'DRA'
-            elif 'brenton' in commentText or 'septuagint' in commentText:
-                return 'Brenton\'s Septuagint'
-            elif 'jps' in commentText or 'tanakh' in commentText:
-                return 'JPS Tanakh'
-            elif 'nova' in commentText or 'vulgata' in commentText or 'nv' in commentText or 'latin' in commentText:
-                return 'Nova Vulgata'
-            elif 'nlt' in commentText:
-                return 'NLT'
-            else: # Uses the default translation for each subreddit
-                if subreddit == 'Catholicism':
-                    return 'DRA'
-                elif subreddit == 'OrthodoxChristianity':
-                    if 40 <= bookNum <= 66:
-                        return 'ESV'
-                    else:
-                        return 'Brenton\'s Septuagint'
-                elif subreddit == 'Judaism':
-                    return 'JPS Tanakh'
-                elif subreddit == 'Protestantism':
-                    return 'KJV'
-                else: # This also includes subreddits that wish to have ESV as the default translation
-                    return 'ESV'
-    
-    # Retrieves the contents of the selected verse(s) from the pickle files loaded at the beginning of the program.
-    # It then constructs the verse contents into part of a comment.
-    def __getVerseContent(self, book = False, chap = False, ver = False, translation = False):
-        verseText = ''
-        currentSelection = ''
-
-        if translation == 'ESV':
-            bible = self._ESV
-        elif translation == 'NIV':
-            bible = self._NIV
-        elif translation == 'NRSV':
-            bible = self._NRSV
-        elif translation == 'KJV' or translation == 'KJV Deuterocanon':
-            bible = self._KJV
-        elif translation == 'DRA':
-            bible = self._DRA
-        elif translation == 'Brenton\'s Septuagint':
-            bible = self._Brenton
-        elif translation == 'JPS Tanakh':
-            bible = self._JPS
-        elif translation == 'Nova Vulgata':
-            bible = self._Nova
-        elif translation == 'NLT':
-            bible = self._NLT
-
-        if book and chap:
-            try:
-                if ver != '0':
-                    if '-' in ver:
-                        startingVer = ver.partition('-')[0]
-                        endingVer = ver.partition('-')[2]
-                        if int(startingVer) < int(endingVer) + 1:
-                            for verse in range(int(startingVer), int(endingVer) + 1):
-                                verseText += '[**' + str(verse) + '**] ' + (bible[str(book)][int(chap)][verse] + ' ')
-                        else:
-                            return False
-                    else:
-                        verseText = '[**' + ver + '**] ' + bible[str(book)][int(chap)][int(ver)]
-                else:
-                    for verse in bible[str(book)][int(chap)]:
-                        verseText += '[**' + str(verse) + '**] ' + (bible[str(book)][int(chap)][verse] + ' ')
-                currentSelection += verseText
-
-                return currentSelection
-            except KeyError:
-                return False
-    
-    # Simply returns the comment footer found at the bottom of every comment posted by the bot.
-    def __getCommentFooter(self):
-        return ('\n***\n[[Source Code](https://github.com/matthieugrieger/versebot)]'
-               + ' [[Feedback](https://github.com/matthieugrieger/versebot/issues)]' 
-               + ' [[Contact Dev](http://www.reddit.com/message/compose/?to=mgrieger)]'
-               + ' [[FAQ](https://github.com/matthieugrieger/versebot/blob/master/docs/VerseBot%20Info.md#faq)]' 
-               + ' [[Changelog](https://github.com/matthieugrieger/versebot/blob/master/docs/CHANGELOG.md)]')
-
-    # Takes the verse's book name, chapter, and translation as parameters. The function then constructs
-    # the appropriate context link. This link appears on each verse title.
-    def __getContextLink(self, bookName, chap, translation):
-        if translation == 'Brenton\'s Septuagint':
-            link = ('http://studybible.info/Brenton/' + bookName + '%20' + chap).replace(' ', '%20')
-        elif translation == 'KJV Deuterocanon':
-            link = ('http://kingjamesbibleonline.org/' + bookName + '-Chapter-' + chap + '/').replace(' ', '-')
-        elif translation == 'JPS Tanakh':
-            link = ('http://www.taggedtanakh.org/Chapter/Index/english-' + booknames.getTanakhName(bookName) + '-' + chap)
-        elif translation == 'Nova Vulgata':
-            novaNum = booknames.getBookNumber(bookName.lower())
-            if novaNum <= 39: # The Vatican website URL is different for Old Testament (Vetus Testamentum) and New Testament (Novum Testamentum)
-                if novaNum == 19: # Links to Psalms are formatted differently on http://vatican.va/
-                    link = ('http://www.vatican.va/archive/bible/nova_vulgata/documents/nova-vulgata_vt_psalmorum_lt.html#PSALMUS%20' + chap)
-                else:
-                    link = ('http://www.vatican.va/archive/bible/nova_vulgata/documents/nova-vulgata_vt_' + booknames.getNovaName(bookName) + '_lt.html#' + chap)
-            else:
-                link = ('http://www.vatican.va/archive/bible/nova_vulgata/documents/nova-vulgata_nt_' + booknames.getNovaName(bookName) + '_lt.html#' + chap)
-        else:
-            link = ('http://www.biblegateway.com/passage/?search=' + bookName + '%20' + chap + '&version=' + translation).replace(' ', '%20')
-
-        return link
-
-    # Constructs and returns an overflow comment whenever the comment exceeds the character limit set by
-    # __getCharLimit(). Instead of posting the contents of the verse(s) in the comment, it links to webpages
-    # that contain the contents of the verse(s).
-    def __getOverflowComment(self):
-        comment = 'The contents of the verse(s) you quoted exceed the character limit (4000 characters). Instead, here are links to the verse(s)!\n\n'
-        for curVerData in self._verseData:
-            book = curVerData[0]
-            chap = curVerData[1]
-            ver = curVerData[2]
-            translation = curVerData[3]
-
-            if translation == 'Brenton\'s Septuagint':
-                if ver != '0':
-                    overflowLink = ('http://www.studybible.info/Brenton/' + book + '%20' + chap + ':' + ver).replace(' ', '%20')
-                else:
-                    overflowLink = ('http://www.studybible.info/Brenton/' + book + '%20' + chap).replace(' ', '%20')
-            elif translation == 'KJV Deuterocanon':
-                 if ver != '0':
-                    overflowLink = ('http://www.kingjamesbibleonline.org/' + book + '-' + chap + '-' + ver + '/').replace(' ', '-')
-                 else:
-                    overflowLink = ('http://www.kingjamesbibleonline.org/' + book + '-Chapter-' + chap + '/').replace(' ', '-')
-            elif translation == 'JPS Tanakh':
-                 overflowLink = ('http://www.taggedtanakh.org/Chapter/Index/english-' + booknames.getTanakhName(book) + '-' + chap)
-            elif translation == 'Nova Vulgata':
-                overflowLink = self.__getContextLink(book, chap, translation)
-            else:
-                if ver != '0':
-                    overflowLink = ('http://www.biblegateway.com/passage/?search=' + book + '%20' + chap + ':' + 
-                                    ver + '&version=' + translation).replace(' ', '%20')
-                else:
-                    overflowLink = ('http://www.biblegateway.com/passage/?search=' + book + '%20' + chap + 
+	
+	_verse_data = list()
+	_invalid_comment = False
+	
+	# Initializes Verse object with data from the command(s)
+	def __init__(self, verse_list, comment):
+		for verse in verse_list:
+			verse_book_num = data.get_book_number(verse.lower())
+			if verse_book_num != False:
+				verse_subreddit = comment.permalink[24:comment.permalink.find('/', 24)]
+				verse_book_name = data.get_book_title(verse_book_num)
+				verse_chapter = self._get_chapter_and_verse(verse.lower())[0]
+				verse_selection = self._get_chapter_and_verse(verse.lower())[1]
+				verse_translation = self._get_bible_translation(verse.upper(), verse_subreddit, verse_book_num)
+				verse_content = helpers.get_verse_contents(verse_book_name, verse_chapter, verse_selection, verse_translation)
+				verse_title = helpers.get_verse_title()
+				trans_title = helpers.get_translation_title()
+				
+				if verse_content != False:
+					self._verse_data.append((verse_book_name, verse_chapter, verse_selection, verse_translation, verse_content, verse_subreddit, verse_title, trans_title))
+		
+		if len(self._verse_data) == 0:
+			self._invalid_comment = True
+			
+		return
+	
+	# Constructs reddit comment.	
+	def get_comment(self):
+		if not self._invalid_comment:
+			comment = ''
+			for cur_ver_data in self._verse_data:
+				book = cur_ver_data[0]
+				chap = cur_ver_data[1]
+				ver = cur_ver_data[2]
+				translation = cur_ver_data[3]
+				content = cur_ver_data[4]
+				subreddit = cur_ver_data[5]
+				context_link = self._get_context_link(book, chap, translation)
+				verse_title = cur_ver_data[6]
+				trans_title = cur_ver_data[7]
+				
+				comment += ('[**' + verse_title.lstrip() + ' | ' + trans_title.lstrip() + '**](' + context_link + ')\n>' + content) + '\n\n'
+								
+			if 0 < len(comment) <= self._get_char_limit():
+				comment += self._get_comment_footer()
+				return comment
+			else:
+				comment = self._get_overflow_comment()
+				comment += self._get_comment_footer()
+				return comment
+		else:
+			return False
+	
+	# Clears contents of _verseData.		
+	def clear_verses(self):
+		self._verse_data[:] = []
+		return
+		
+	def get_verse_data(self):
+		return self._verse_data
+	
+	# Finds chapter number and verse number within current verse request.
+	def _get_chapter_and_verse(self, verse):
+		chap = '0'
+		ver = '0'
+		
+		if ':' in verse:
+			chapter_and_verse = str(findall(r'\d+:\d*(?:-\d+)?', verse))
+			chap = (chapter_and_verse.partition(':')[0])[2:]
+			ver = (chapter_and_verse.partition(':')[2])[:-2]
+		else:
+			chap = str(findall(r'\s\d+', verse)).lstrip(' ')
+			chap = chap[3:-2]
+			
+		return chap, ver
+		
+	# Determines the correct Bible translation to use for the current verse.
+	# It first looks for a user-specified translation. If there is no
+	# translation specified, it will then use the default translation
+	# for the subreddit in which the comment was posted.
+	def _get_bible_translation(self, comment_text, subreddit, book_num):
+		for translation in helpers.get_supported_translations():
+			if search(r'\b' + translation + r'\b', comment_text):
+				return translation
+		return data.get_default_translation(subreddit, book_num)
+	
+	# Simply returns the comment footer found at the bottom of every comment posted
+	# by the bot.		
+	def _get_comment_footer(self):
+		return ('\n***\n[^Source ^Code](https://github.com/matthieugrieger/versebot) ^|'
+               + ' [^/r/VerseBot](http://www.reddit.com/r/versebot) ^|' 
+               + ' [^Contact ^Dev](http://www.reddit.com/message/compose/?to=mgrieger) ^|'
+               + ' [^FAQ](https://github.com/matthieugrieger/versebot/blob/master/docs/VerseBot%20Info.md#faq) ^|' 
+               + ' [^Changelog](https://github.com/matthieugrieger/versebot/blob/master/docs/CHANGELOG.md) \n\n'
+               + ' ^All ^texts ^provided ^by [^BibleGateway](http://www.biblegateway.com)')
+	
+	# Takes the verse's book name, chapter, and translation as parameters. The function
+	# then constructs a context link for the selected passage. This link appears on each
+	# verse title.
+	def _get_context_link(self, book_name, chap, translation):
+		return ('http://www.biblegateway.com/passage/?search=' + book_name + '%20' + chap
+				+ '&version=' + translation).replace(' ', '%20')
+	
+	# Constructs and returns an overflow comment whenever the comment exceeds the character
+	# limit set by _get_char_limit(). Instead of posting the contents of the verse(s) in the comment,
+	# it links to webpages that contain the contents of the verse(s).			
+	def _get_overflow_comment(self):
+		comment = 'The contents of the verse(s) you quoted exceed the character limit (' + str(self._get_char_limit()) + ' characters). Instead, here are links to the verse(s)!\n\n'
+		for cur_ver_data in self._verse_data:
+			book = cur_ver_data[0]
+			chap = cur_ver_data[1]
+			ver = cur_ver_data[2]
+			translation = cur_ver_data[3]
+			
+			if ver != '0':
+				overflow_link = ('http://www.biblegateway.com/passage/?search=' + book + '%20' + chap + ':' + 
+								ver + '&version=' + translation).replace(' ', '%20')
+				comment += ('- [' + book + ' ' + chap + ':' + ver + ' (' + translation + ')](' + overflow_link + ')\n')
+			else:
+				overflow_link = ('http://www.biblegateway.com/passage/?search=' + book + '%20' + chap + 
                                  '&version=' + translation).replace(' ', '%20')
-
-            if ver != '0':
-                comment += ('- [' + book + ' ' + chap + ':' + ver + ' (' + translation + ')](' + overflowLink + ')\n')
-            else:
-                comment += ('- [' + book + ' ' + chap + ' (' + translation + ')](' + overflowLink + ')\n')
-        
-        return comment
-    
-    # Just returns the current character limit for the reddit comment. Makes it easy to find/change in the future.
-    # NOTE: reddit's character limit is 10,000 characters by default.
-    def __getCharLimit(self):
-        return 4000
-
-
-
+				comment += ('- [' + book + ' ' + chap + ' (' + translation + ')](' + overflow_link + ')\n')
+		
+		return comment
+	
+	# Simply returns the current character limit for the reddit comment. Makes it easy to
+	# find/change in the future. NOTE: reddit's character limit is 10,000 characters by default.	
+	def _get_char_limit(self):
+		return 6000
--- versebot/versebot.py
+++ versebot/versebot.py
@@ -3,134 +3,92 @@
 # By Matthieu Grieger #
 #---------------------#
 
-import pickle
-import configloader
 import praw
-import psycopg2
-import urllib.parse
+import config
+import database
+import requests
+from helpers import find_supported_translations
 from verse import Verse
+
 from sys import exit
 from re import findall
 from time import sleep
 from os import environ
-from warnings import filterwarnings
 
-# Ignores ResourceWarnings when using pickle files. May need to look into this later, but it seems to work fine.
-filterwarnings("ignore", category=ResourceWarning)
-# Ignores DeprecationWarnings caused by PRAW
-filterwarnings("ignore", category=DeprecationWarning) 
-
-print('Starting up VerseBot...')
-
-# Loads Bible translation pickle files into memory.
-print('Loading Bible translations...')
-try:
-    esvbible = pickle.load(open(configloader.getESV(), 'rb'))
-    nivbible = pickle.load(open(configloader.getNIV(), 'rb'))
-    nrsvbible = pickle.load(open(configloader.getNRSV(), 'rb'))
-    kjvbible = pickle.load(open(configloader.getKJV(), 'rb'))
-    drabible = pickle.load(open(configloader.getDRA(), 'rb'))
-    brentonbible = pickle.load(open(configloader.getBrenton(), 'rb'))
-    jpsbible = pickle.load(open(configloader.getJPS(), 'rb'))
-    novabible = pickle.load(open(configloader.getNova(), 'rb'))
-    nltbible = pickle.load(open(configloader.getNLT(), 'rb'))
-    bibles = (esvbible, nivbible, nrsvbible, kjvbible, drabible, brentonbible, jpsbible, novabible, nltbible)
-    print('Bible translations successfully loaded!')
-except:
-    print('Error while loading Bible translations. Make sure the environment vars point to correct paths.')
-    exit()
-
-# Connects to reddit via PRAW.
-print('Connecting to reddit...')
-try:
-    r = praw.Reddit(user_agent='VerseBot by /u/mgrieger. Github: https://github.com/matthieugrieger/versebot')
-    r.login(configloader.getBotUsername(), configloader.getBotPassword())
-    print('Connected to reddit!')
-except:
-    print('Connection to reddit failed. Either reddit is down at the moment, or something in the config is incorrect.')
-    exit()
-
-# Connects to a PostgreSQL database used to store comment ids.
-print('Connecting to database...')
-urllib.parse.uses_netloc.append('postgres')
-url = urllib.parse.urlparse(environ['HEROKU_POSTGRESQL_ONYX_URL'])
-try:
-    conn = psycopg2.connect(
-    database = url.path[1:],
-    user = url.username,
-    password = url.password,
-    host = url.hostname,
-    port = url.port)
+def main():
+    print('Starting up VerseBot...')
     
-    cur = conn.cursor()
-    print('Connected to database!')
-except:
-    print('Connection to database failed.')
-    exit()
-
-# Fills text file previous comment ids from PostgreSQL database.
-print('Setting up tmp.txt...')
-try:
-    io = open('tmp.txt', 'w')
-    cur.copy_to(io, 'commentids', sep='|')
-    io.close()
-    print('tmp.txt ready!')
-except:
-    print('Error when setting up tmp.txt.')
-    exit()
-
-commentsAdded = False
-lookupList = list()
-comment_ids_this_session = set() # This is to help protect against spamming when connection to database is lost.
-
-print('Beginning to scan comments...')
-# This loop runs every 30 seconds.
-while True:
-    if commentsAdded:
-        # Copies new comment ids from database into txt file for searching.
-        io = open('tmp.txt', 'w')
-        cur.copy_to(io, 'commentids', sep='|')
-        io.close()
-    subreddit = r.get_subreddit(configloader.getSubreddits())
-    subreddit_comments = subreddit.get_comments()
-
-    for comment in subreddit_comments:
-        if comment.author != configloader.getBotUsername() and comment.id not in open('tmp.txt').read() and comment.id not in comment_ids_this_session:
-            comment_ids_this_session.add(comment.id)
-            versesToFind = findall(r'\[[\w\s:,-]+](?!\()', comment.body) # Uses regex to find potential verses in comment body.
-            if len(versesToFind) != 0:
-                for ver in versesToFind:
-                    # This regex is ugly, I will look into making it prettier later.
-                    nextVer = findall(r'(?:\d\w*\s)?(?:\w+\s\w+\s\w+)?(?:\w+\s\w+\s\w+\s\w+)?\w+\s\d+:?\d*-?\d*(?:\s\w+)?', ver)
-                    lookupList.append(str(nextVer))
-
-                if len(lookupList) != 0:
-                    verseObject = Verse(lookupList, comment, bibles)
-                    nextComment = verseObject.getComment()
-                    if nextComment != False:
-                        comment.reply(nextComment)
-                    verseObject.clearVerses()
-                else:
-                    nextComment = False
-
-                if nextComment != False:
-                    print('Inserting new comment id to database...')
-                    try:
-                        cur.execute("""INSERT INTO commentids VALUES (%s);""", (comment.id,))
-                        conn.commit()
-                    except:
-                        print('Database insert failed.')
-                        exit()
-                    commentsAdded = True
-                    lookupList.clear()
-                else:
-                    commentsAdded = False
-                    try:    
-                        # Removes comment id from set if the comment was not replied to. This should prevent situations where the comment
-                        # has a valid command and the bot does not reply because the reply operation failed the first time through.
-                        comment_ids_this_session.remove(comment.id)
-                    except KeyError:
-                        pass
-                    lookupList.clear()
+    # Connects to reddit via PRAW.
+    try:
+        r = praw.Reddit(user_agent = ('VerseBot by /u/mgrieger. Github: https://github.com/matthieugrieger/versebot'))
+        r.login(config.get_bot_username(), config.get_bot_password())
+        print('Connected to reddit!')
+    except:
+        print('Connection to reddit failed. Either reddit is down at the moment or something in the config is incorrect.')
+        exit()
     
-    sleep(30)
+    print('Connecting to database...')
+    database.connect()
+    print('Cleaning database...')
+    database.clean_comment_id_database()
+    
+    print('Retrieving supported translations...')
+    find_supported_translations()
+    
+    lookup_list = list()
+    comment_ids_this_session = set()
+    
+    print('Beginning to scan comments...')
+    while True:
+		comments = praw.helpers.comment_stream(r, 'all', limit=None)
+		for comment in comments:
+			if comment.author != config.get_bot_username() and not database.check_comment_id(comment.id) and comment.id not in comment_ids_this_session:
+				comment_ids_this_session.add(comment.id)
+				verses_to_find = findall(r'\[[\w\s:,-]+](?!\()', comment.body)
+				if len(verses_to_find) != 0:
+					for ver in verses_to_find:
+						next_ver = findall(r'(?:\d\w*\s)?(?:\w+\s\w+\s\w+)?(?:\w+\s\w+\s\w+\s\w+)?\w+\s\d+:?\d*-?\d*(?:\s\w+-?\w*)?', ver)
+						lookup_list.append(str(next_ver))
+					
+					if len(lookup_list) != 0:
+						verse_object = Verse(lookup_list, comment)
+						next_comment = verse_object.get_comment()
+						if next_comment != False:
+							try:
+								comment.reply(next_comment)
+							except requests.exceptions.HTTPError, err:
+								# If true, this means the bot is banned.
+								if str(err) == '403 Client Error: Forbidden':
+									print('Banned from subreddit. Cannot reply.')
+									next_comment = False
+							except praw.errors.APIException, err:
+								if err.error_type in ('TOO_OLD','DELETED_LINK'):
+									next_comment = False
+								else:
+									raise
+					else:
+						next_comment = False
+					
+					if next_comment != False:
+						print('Inserting new comment id to database...')
+						database.add_comment_id(comment.id)
+						print('Updating statistics...')
+						database.update_db_stats(verse_object)
+						
+						lookup_list[:] = []
+					else:
+						try:
+							comment_ids_this_session.remove(comment.id)
+						except KeyError:
+							pass
+						lookup_list[:] = []
+					verse_object.clear_verses()
+    
+if __name__ == '__main__':
+	main()
+
+
+    
+        
+        
+    
