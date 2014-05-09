7f4b7aef5adf30a225842cc53338c35e1b87ae56
Matthieu Grieger <matthieugrieger@gmail.com>
2014-05-08 19:19:35 -0700

    Add config.py (replaces configloader.py)

---
+++ versebot/config.py
@@ -0,0 +1,15 @@
+#---------------------#
+# VerseBot for reddit #
+# By Matthieu Grieger #
+#---------------------#
+
+from os import environ
+
+def get_bot_username():
+    return environ['USERNAME']
+
+def get_bot_password():
+    return environ['PASSWORD']
+    
+def get_database():
+	return environ['HEROKU_POSTGRESQL_ONYX_URL']
