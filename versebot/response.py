e3437290031a5a3325cd3eac0db65cd41ccef470
Matthieu Grieger <matthieugrieger@gmail.com>
2015-01-19 22:26:27 -0800

    Laid out the Response and Verse classes

--- versebot/response.py
+++ versebot/response.py
@@ -4,3 +4,18 @@ By Matthieu Grieger
 response.py
 Copyright (c) 2015 Matthieu Grieger (MIT License)
 """
+
+class Response:
+    """ Class that holds the properties and methods of a comment 
+    response. """
+    
+    def __init__(self, message):
+        """ Initializes a Response object. """
+        self._verse_list = list()
+        
+    def add_verse(verse):
+        """ Adds a verse to the verse list. """
+        self._verse_list.append(verse)
+        
+    def construct_message(self):
+        """ Constructs a message response. """
--- versebot/verse.py
+++ versebot/verse.py
@@ -4,3 +4,14 @@ By Matthieu Grieger
 verse.py
 Copyright (c) 2015 Matthieu Grieger (MIT License)
 """
+
+class Verse:
+    """ Class that holds the properties and methods of a Verse object. """
+    
+    def __init__(self, book, chapter, verse, translation):
+        """ Initializes a Verse object with book, chapter, verse (if
+        exists), and translation (if exists). """
+        self.book = book
+        self.chapter = chapter
+        self.verse = verse
+        self.translation = translation
