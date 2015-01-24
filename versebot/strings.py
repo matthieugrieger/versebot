307a9a3879ab61864e7aafc55f81dc6cbf46c4af
Matthieu Grieger <matthieugrieger@gmail.com>
2015-01-24 14:21:02 -0800

    Add NO_VERSE_FOUND_SUBJECT

--- versebot/strings.py
+++ versebot/strings.py
@@ -5,6 +5,7 @@ strings.py
 Copyright (c) 2015 Matthieu Grieger (MIT License)
 """
 
+NO_VERSE_FOUND_SUBJECT = "No verse found!"
 NO_VERSE_FOUND_MSG = ("A verse could not be found in your comment. Please take a moment to"
     + " review the VerseBot usage guidelines if you haven't already.\n\nThink this is a mistake?"
     + " [Send a message to the developer, and he will look into it!](http://www.reddit.com/message/compose/?to=mgrieger)")
