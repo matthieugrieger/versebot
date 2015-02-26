15fdae00186d04246c550450ed68f136b3d8149e
Matthieu Grieger <matthieugrieger@gmail.com>
2015-02-25 23:08:09 -0800

    Added response functions to VerseBot class

--- versebot/versebot.py
+++ versebot/versebot.py
@@ -7,14 +7,13 @@ Copyright (c) 2015 Matthieu Grieger (MIT License)
 
 import praw
 import database
-import regex
 import logging
 from config import *
-from strings import *
 from time import sleep
-from response import Response
-from verse import Verse
 from webparser import WebParser
+from verse import Verse
+from regex import find_verses
+from response import Response
 
 class VerseBot:
     """ Main VerseBot class. """
@@ -45,24 +44,44 @@ class VerseBot:
             messages = self.r.get_unread()
             for message in messages:
                 if message.subject == "username mention":
-                    verses = regex.find_verses(message.body)
-                    if verses != None:
-                        response = Response(message)
-                        for verse in verses:
-                            response.add_verse(Verse(verse[0], verse[1], verse[2], verse[3]))
-                        self.log.info("Replying to %s with verse quotations..." % message.author)
-                        message.reply(response.construct_message())
-                    else:
-                        self.log.info("No verses found in this message. Forwarding to /u/%s..." % VERSEBOT_ADMIN)
-                        self.r.send_message(VERSEBOT_ADMIN, "Forwarded VerseBot Message", 
-                            "%s\n\n[[Link to Original Message](%s)]" % (message.body, message.permalink))
+                    messages.respond_to_username_mention(message)
                 elif message.subject == "Edit Request":
-                    pass
+                    messages.respond_to_edit_request(message)
                 elif message.subject == "Delete Request":
-                    pass
+                    messages.respond_to_delete_request(message)
                 message.mark_as_read()
             sleep(30)
             
+    def respond_to_username_mention(self, message):
+        """ Responds to a username mention. This could either contain one or more valid
+        Bible verse quotation requests, or it could simply be a username mention without
+        any valid Bible verses. If there are valid Bible verses, VerseBot generates a
+        response that contains the text from these quotations. Otherwise, the message is
+        forwarded to the VerseBot admin for review. """
+        
+        verses = find_verses(message.body)
+        if verses != None:
+            response = Response(message)
+            for verse in verses:
+                response.add_verse(Verse(verse[0], verse[1], verse[2], verse[3]))
+            self.log.info("Replying to %s with verse quotations..." % message.author)
+            message.reply(response.construct_message())
+        else:
+            self.log.info("No verses found in this message. Forwarding to /u/%s..." % VERSEBOT_ADMIN)
+            self.r.send_message(VERSEBOT_ADMIN, "Forwarded VerseBot Message", 
+                "%s\n\n[[Link to Original Message](%s)]" % (message.body, message.permalink))
+                    
+    def respond_to_edit_request(self, message):
+        """ Responds to an edit request. The bot will parse the body of the message, looking for verse
+        quotations. These will replace the quotations that were placed in the original response to the
+        user. Once the comment has been successfully edited, the bot then sends a message to the user
+        letting them know that their verse quotations have been updated. """
+        
+    def respond_to_delete_request(self, message):
+        """ Responds to a delete request. The bot will attempt to open the comment which has been requested
+        to be deleted. If the submitter of the delete request matches the author of the comment that triggered
+        the VerseBot response, the comment will be deleted. The bot will then send a message to the user letting
+        them know that their verse quotation comment has been removed. """
 
 bot = VerseBot(REDDIT_USERNAME, REDDIT_PASSWORD)
 if __name__ == "__main__":
