ec3757ef1704383726dc9abb880a99066b763256
Matthieu Grieger <matthieugrieger@gmail.com>
2015-02-27 15:30:36 -0800

    Added translation detection

--- versebot/verse.py
+++ versebot/verse.py
@@ -5,20 +5,48 @@ verse.py
 Copyright (c) 2015 Matthieu Grieger (MIT License)
 """
 
+import database
+import books
+
 class Verse:
     """ Class that holds the properties and methods of a Verse object. """
-    
-    def __init__(self, book, chapter, translation, verse="0"):
+
+    def __init__(self, book, chapter, translation, user, subreddit, verse="0"):
         """ Initializes a Verse object with book, chapter, verse (if
         exists), and translation (if exists). """
-        self.book = book.lower().replace(" ", "")
+        self.book = book
+        book_num = books.get_book_number(self.book)
+        if book_num <= 39:
+            self.bible_section = "Old Testament"
+        else if book_num <= 66:
+            self.bible_section = "New Testament"
+        else:
+            self.bible_section = "Deuterocanon"
+
         self.chapter = int(chapter.replace(" ", ""))
         self.verse = verse.replace(" ", "")
-        self.translation = translation.replace(" ", "")
+        if translation:
+            self.translation = translation.replace(" ", "")
+        else:
+            user_default = database.get_user_default_translation(user, self.bible_section)
+            if user_default:
+                self.translation = user_default
+            else:
+                subreddit_default = database.get_subreddit_default_translation(subreddit, self.bible_section)
+                if subreddit_default:
+                    self.translation = subreddit_default
+                else:
+                    if self.bible_section == "Old Testament":
+                        self.translation = "ESV"
+                    else if self.bible_section == "New Testament":
+                        self.translation = "ESV"
+                    else:
+                        self.translation = "NRSV"
+
         self.verse_title = ""
         self.translation_title = ""
         self.contents = ""
-        
+
     def get_contents(self, parser):
         """ Retrieves the contents of a Verse object if it exists in
         the supported translation list. """
--- versebot/versebot.py
+++ versebot/versebot.py
@@ -8,6 +8,7 @@ Copyright (c) 2015 Matthieu Grieger (MIT License)
 import praw
 import database
 import logging
+import books
 from config import *
 from time import sleep
 from webparser import WebParser
@@ -17,7 +18,7 @@ from response import Response
 
 class VerseBot:
     """ Main VerseBot class. """
-    
+
     def __init__(self, username, password):
         """ Initializes a VerseBot object with supplied username and password. It is recommended that
         the username and password are stored in something like an environment variable for security
@@ -36,7 +37,7 @@ class VerseBot:
         database.connect(self.log)  # Initialize connection to database.
         self.log.info("Successfully connected to database!")
         self.parser = WebParser()  # Initialize web parser with updated translation list.
-    
+
     def main_loop(self):
         """ Main inbox searching loop for finding verse quotation requests. """
         self.log.info("Beginning to scan for new inbox messages...")
@@ -51,38 +52,45 @@ class VerseBot:
                     messages.respond_to_delete_request(message)
                 message.mark_as_read()
             sleep(30)
-            
+
     def respond_to_username_mention(self, message):
         """ Responds to a username mention. This could either contain one or more valid
         Bible verse quotation requests, or it could simply be a username mention without
         any valid Bible verses. If there are valid Bible verses, VerseBot generates a
         response that contains the text from these quotations. Otherwise, the message is
         forwarded to the VerseBot admin for review. """
-        
+
         verses = find_verses(message.body)
         if verses != None:
             response = Response(message, self.parser)
             for verse in verses:
-                response.add_verse(Verse(verse[0], verse[1], verse[2], verse[3]))
-            self.log.info("Replying to %s with verse quotations..." % message.author)
-            message.reply(response.construct_message(self.parser))
+                book_name = books.get_book(verse[0].lower().replace(" ", ""))
+                if book_name:
+                    response.add_verse(Verse(book_name,  # Book
+                        verse[1],  # Chapter
+                        verse[3],  # Translation
+                        message.author,  # User
+                        message.permalink[24:message.permalink.find("/", 24)]  # Subreddit
+                        verse[2]))  # Verse
+                    self.log.info("Replying to %s with verse quotations..." % message.author)
+                    message.reply(response.construct_message(self.parser))
         else:
             self.log.info("No verses found in this message. Forwarding to /u/%s..." % VERSEBOT_ADMIN)
-            self.r.send_message(VERSEBOT_ADMIN, "Forwarded VerseBot Message", 
+            self.r.send_message(VERSEBOT_ADMIN, "Forwarded VerseBot Message",
                 "%s\n\n[[Link to Original Message](%s)]" % (message.body, message.permalink))
-                    
+
     def respond_to_edit_request(self, message):
         """ Responds to an edit request. The bot will parse the body of the message, looking for verse
         quotations. These will replace the quotations that were placed in the original response to the
         user. Once the comment has been successfully edited, the bot then sends a message to the user
         letting them know that their verse quotations have been updated. """
-        
+
     def respond_to_delete_request(self, message):
         """ Responds to a delete request. The bot will attempt to open the comment which has been requested
         to be deleted. If the submitter of the delete request matches the author of the comment that triggered
         the VerseBot response, the comment will be deleted. The bot will then send a message to the user letting
         them know that their verse quotation comment has been removed. """
-        
+
 
 bot = VerseBot(REDDIT_USERNAME, REDDIT_PASSWORD)
 if __name__ == "__main__":
