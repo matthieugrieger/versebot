b0c613f50a06a4b715eab3325566c939b335c365
Matthieu Grieger <matthieugrieger@gmail.com>
2015-03-23 22:26:01 -0700

    Database update files completed

--- sql/update_old_db/update_timestamp_columns.sql
+++ sql/update_old_db/update_timestamp_columns.sql
@@ -5,5 +5,11 @@
   Copyright (c) 2015 Matthieu Grieger (MIT License)
 */
 
-UPDATE books_stats last_used = NULL WHERE count = 0;
-UPDATE translation_stats last_used = NULL WHERE count = 0;
+ALTER TABLE book_stats DISABLE TRIGGER update_book_stats_timestamp;
+ALTER TABLE translation_stats DISABLE TRIGGER update_translation_stats_timestamp;
+
+UPDATE book_stats SET last_used = NULL WHERE count = 0;
+UPDATE translation_stats SET last_used = NULL WHERE count = 0;
+
+ALTER TABLE book_stats ENABLE TRIGGER update_book_stats_timestamp;
+ALTER TABLE translation_stats ENABLE TRIGGER update_translation_stats_timestamp;
